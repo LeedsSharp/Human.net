37dbf371b6fc766173017df643c6618214624996
Matt Ross <matt@tronik.co.uk>
2015-03-27 11:00:06 +0000

    Added test project for Cardiovascular System and branching off Human.

---
+++ CardiovascularSystem.Tests/HeartTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace CardiovascularSystem.Tests
+{
+    [TestFixture]
+    public class HeartTests
+    {
+        #region Setup
+
+        [TestFixtureSetUp]
+        public void SetUpOnce()
+        {
+
+        }
+
+        [TestFixtureTearDown]
+        public void TearDownOnce()
+        {
+
+        }
+
+        [SetUp]
+        public void SetupBeforeEveryTest()
+        {
+
+        }
+
+        [TearDown]
+        public void TearDownBeforeEveryTest()
+        {
+
+        }
+
+        #endregion
+
+        #region Tests
+
+         
+
+        #endregion
+    }
+}
